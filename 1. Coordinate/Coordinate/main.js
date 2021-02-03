86305c73e2877904b846950b96a146b3bdb0d3ba
helloworld-jisoo <helloworld.jisoo@gmail.com>
2021-02-03 17:18:16 +0000

    'load' (after all resources(css,image..) are ready)

--- 1. Coordinate/Coordinate/main.js
+++ 1. Coordinate/Coordinate/main.js
@@ -3,6 +3,9 @@ const lineX = document.querySelector(".X");
 const lineY = document.querySelector(".Y");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
+
+addEventListener('load',()=> {
+  
 const targetRect = target.getBoundingClientRect();
 const targetHalfWidth = targetRect.width/2;
 const targetHalfHeight = targetRect.height/2;
@@ -10,11 +13,12 @@ const targetHalfHeight = targetRect.height/2;
 document.addEventListener('mousemove', (event) => {
   const x = event.clientX;
   const y = event.clientY;
-
+  
   lineY.style.transform = `translateX(${x}px)`
   lineX.style.transform = `translateY(${y}px)`
-  target.style.transform = `translate(${ x - targetHalfWidth}px, ${ y - targetHalfHeight}px)`
+  target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`
   tag.style.transform = `translate(${x}px, ${y}px)`
 
   tag.innerHTML = `${x}px ${y}px`
 });
+})

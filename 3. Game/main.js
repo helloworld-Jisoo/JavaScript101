5424468d7d819e0d94ba6c155f7e94e3daf75f86
helloworld-jisoo <helloworld.jisoo@gmail.com>
2021-02-09 08:01:37 +0000

    Event Delegation
    field.addEventListener('click', onFieldEvent)
    event handler for carrots and bug => use event delegation(parent element: field)

--- 3. Game/main.js
+++ 3. Game/main.js
@@ -19,7 +19,8 @@ const fieldRect = field.getBoundingClientRect();
 let started = false;
 let score = 0;
 let time = undefined;
-
+// 이벤트 위임을 이용해서 field 안에서 클릭이 발생하면 어떤것이 클릭 됬냐의 따라 기능 수행
+field.addEventListener('click', onFieldClick); // = ('click', (event)=>onFieldClick(event));
 playBtn.addEventListener('click', ()=>{
   // 만약 게임이 시작이 되었다면, 게임을 중지해야 하고 게임이 시작되지 않았다면 게임을 시작해야함.
   if(started) {
@@ -96,6 +97,36 @@ function initGame(){
   addItem('carrot',CARROT_COUNT,'img/carrot.png');
   addItem('bug',BUG_COUNT,'img/bug.png');
 }
+function onFieldClick(event) {
+  console.log(event);
+  if(!started) {
+    return; // 게임이 시작되지 않으면 함수를 나갈꺼임 (*조건이 맞지 않을때 빨리 함수를 return하는 것이 중요함)
+  } 
+  //이제 원하는 기능 수행
+  const target = event.target;
+  if (target.matches('.carrot')){ // matches란 함수는 css selector가 해당하는지 확인
+    target.remove(); // 당근을 없앰
+    score++; // score 점수 추가
+    updateScoreBoard(); // UI에 점수 보여주기
+    if(score === CARROT_COUNT){ // score 가 5점이 되면
+      finishGame(true); // 게임 승리
+    }
+  } else if (target.matches('.bug')){
+    stopGameTimer(); // 게임 타이머 멈춤
+    finishGame(false); //게임 패배
+
+  }
+
+  function updateScoreBoard(){
+    remain.innerText = CARROT_COUNT-score;
+  }
+}
+
+function finishGame(win){
+  started = false;
+  hideGameBtn();
+  showPopUp(win? 'You won' : 'You lost');
+}
 
 // * 함수+인자설정 으로 동일한 일을 할 수 있도록 만들어줌
 // * (createBug,createCarrot과 같이 함수를 중복해서 만들 필요가 없음)
@@ -128,4 +159,5 @@ function addItem(className, count, imgPath){
 function randomNumber(min,max){
   return Math.random() * (max-min) + min;
 }
+
 
